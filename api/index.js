71498fa5098b9a874dfbd1c366314979fef03b5c
sarthak2057 <shrutikc59@gmail.com>
2023-10-18 15:31:51 +0545

    mvcsetup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import mongoose from 'mongoose'
+import userRouter from './routes/user.route.js'
 
 dotenv.config()
 
@@ -15,6 +16,7 @@ mongoose
 
 const app = express()
 
+app.use('/api/user', userRouter)
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
 })
